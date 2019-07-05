1b20e9fc99ff6d501b13435ba4cb588cfa485260
Hassan Javed <hassanhafizdev@gmail.com>
2019-07-05 11:58:02 +0500

    added index.html to show flights through html

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Flight
 # Create your views here.
 def index(request):
-    return HttpResponse("Flights")
+    context = {
+        "flights": Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
